9aac9e2ddfb09a13ce3431185dd1e14267e10b59
Apoorv Semwal <apoorv.semwal20@gmail.com>
2020-02-04 23:13:45 -0500

    Added new col - 'Time to resolve'

--- 311_Service_Request_Analysis/src/data_cleaning/DataCleaner.py
+++ 311_Service_Request_Analysis/src/data_cleaning/DataCleaner.py
@@ -23,9 +23,15 @@ def remove_space_from_col_names(df_311):
             .withColumnRenamed('Complaint Type', 'Complaint_Type')
             .withColumnRenamed('Open Data Channel Type', 'Open_Data_Channel_Type'))
 
+def drop_empty_null_values(df_311):
+    df_311 = df_311.filter(df_311.Closed_Date != "" & df_311.City != "")
 
 def calculate_time_to_resolve(df_311):
+    df_311 = df_311.filter(df_311.Closed_Date != "")
     time_fmt = "dd/MM/yyyy HH:mm:ss"
-    time_diff = (f.unix_timestamp('Closed_Date', format=time_fmt)
-                 - f.unix_timestamp('Created_Date', format=time_fmt))
-    return df_311.withColumn("time_to_resolve", time_diff)
+    time_fmt2 = "MM/dd/yyyy HH:mm:ss"
+    timeDiff = f.when(f.to_timestamp(df_311.Closed_Date, time_fmt).isNull(),
+                      f.unix_timestamp('Closed_Date', format=time_fmt2) - f.unix_timestamp('Created_Date',
+                                                                                           format=time_fmt)).otherwise(
+        f.unix_timestamp('Closed_Date', format=time_fmt) - f.unix_timestamp('Created_Date', format=time_fmt))
+    return df_311.withColumn("time_to_resolve", timeDiff)
