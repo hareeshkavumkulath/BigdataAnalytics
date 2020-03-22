1dd747da9189e3606dc13dd8a970632b13d5861d
Apoorv Semwal <apoorv.semwal20@gmail.com>
2020-03-21 22:32:22 -0400

    Better handling of date time during data analysis.

--- 311_Service_Request_Analysis/src/StasticalAnalysis/Analysis.py
+++ 311_Service_Request_Analysis/src/StasticalAnalysis/Analysis.py
@@ -1,1 +1,6 @@
 # Python source script for Analysis Part
+
+
+def monthly_hourly_analysis(df_311):
+    # Insight 2 : Monthly and Hourly Analysis
+    return df_311
--- 311_Service_Request_Analysis/src/data_cleaning/DataCleaner.py
+++ 311_Service_Request_Analysis/src/data_cleaning/DataCleaner.py
@@ -1,12 +1,13 @@
 # Python source script for any Data Cleaning related functions
+from pyspark.sql.functions import when, col, count, isnull, upper, substring, to_timestamp, unix_timestamp
+from pyspark.sql.types import IntegerType
 
-from pyspark.sql import functions as f
 import Constants
 import Utilities as utilFor311
 
 
 def get_missing_value_count(df_311):
-    return df_311.select([f.count(f.when((f.isnull(c) | (f.col(c) == '')), c)).alias(c) for c in df_311.columns])
+    return df_311.select([count(when((isnull(c) | (col(c) == '')), c)).alias(c) for c in df_311.columns])
 
 
 def drop_unwanted_cols(df_311):
@@ -30,48 +31,91 @@ def remove_space_from_col_names(df_311):
 
 
 def drop_empty_null_values(df_311):
-    df_311 = df_311.filter((f.col('City').isNotNull()) & (f.col('City') != "") & (f.col('Closed_Date').isNotNull()) & (
-            f.col('Closed_Date') != ""))
+    df_311 = df_311.filter((col('City').isNotNull()) & (col('City') != "") & (col('Closed_Date').isNotNull()) & (
+            col('Closed_Date') != ""))
     return df_311
 
 
 def update_burrow_city_from_zip_code(df_311):
     areas = ['City', 'Borough']
     for area in areas:
-        df_311 = df_311.withColumn(area, f.when(
-            (f.col("Incident_Zip") > 10450.0) & (f.col("Incident_Zip") < 10475.0) & (f.col(area).isNull()),
-            "BRONX").otherwise(f.col(area)))
-        df_311 = df_311.withColumn(area, f.when(
-            (f.col("Incident_Zip") > 11200.0) & (f.col("Incident_Zip") < 11240.0) & (f.col(area).isNull()),
-            "BROOKLYN").otherwise(f.col(area)))
-        df_311 = df_311.withColumn(area, f.when(
-            (f.col("Incident_Zip") > 10000.0) & (f.col("Incident_Zip") < 10280.0) & (f.col(area).isNull()),
-            "MANHATTAN").otherwise(f.col(area)))
-        df_311 = df_311.withColumn(area, f.when(
-            (f.col("Incident_Zip") > 10300.0) & (f.col("Incident_Zip") < 10315.0) & (f.col(area).isNull()),
-            "STATEN ISLAND").otherwise(f.col(area)))
-        df_311 = df_311.withColumn(area, f.when(
-            (f.col("Incident_Zip") > 11350.0) & (f.col("Incident_Zip") < 11700.0) & (f.col(area).isNull()),
-            "QUEENS").otherwise(f.col(area)))
+        df_311 = df_311.withColumn(area, when(
+            (col("Incident_Zip") > 10450.0) & (col("Incident_Zip") < 10475.0) & (col(area).isNull()),
+            "BRONX").otherwise(col(area)))
+        df_311 = df_311.withColumn(area, when(
+            (col("Incident_Zip") > 11200.0) & (col("Incident_Zip") < 11240.0) & (col(area).isNull()),
+            "BROOKLYN").otherwise(col(area)))
+        df_311 = df_311.withColumn(area, when(
+            (col("Incident_Zip") > 10000.0) & (col("Incident_Zip") < 10280.0) & (col(area).isNull()),
+            "MANHATTAN").otherwise(col(area)))
+        df_311 = df_311.withColumn(area, when(
+            (col("Incident_Zip") > 10300.0) & (col("Incident_Zip") < 10315.0) & (col(area).isNull()),
+            "STATEN ISLAND").otherwise(col(area)))
+        df_311 = df_311.withColumn(area, when(
+            (col("Incident_Zip") > 11350.0) & (col("Incident_Zip") < 11700.0) & (col(area).isNull()),
+            "QUEENS").otherwise(col(area)))
     return df_311
 
 
 def capitalize_cols(df_311, cols):
     for c in cols:
-        df_311 = df_311.withColumn(c, f.upper(f.col(c)))
+        df_311 = df_311.withColumn(c, upper(col(c)))
     return df_311
 
 
 def format_zip_code(df_311):
-    df_311 = df_311.withColumn('Incident_Zip', f.substring('Incident_Zip', 0, 5).cast('float'))
+    df_311 = df_311.withColumn('Incident_Zip', substring('Incident_Zip', 1, 5).cast('float'))
     return df_311
 
 
 def calculate_time_to_resolve_in_seconds(df_311):
     time_fmt = "dd/MM/yyyy HH:mm:ss"
     time_fmt2 = "MM/dd/yyyy HH:mm:ss"
-    time_diff = f.when(f.to_timestamp(df_311.Closed_Date, time_fmt).isNull(), f.unix_timestamp('Closed_Date',
-                                                                                               format=time_fmt2) - f.unix_timestamp(
+    time_diff = when(to_timestamp(df_311.Closed_Date, time_fmt).isNull(), unix_timestamp('Closed_Date',
+                                                                                         format=time_fmt2) - unix_timestamp(
         'Created_Date', format=time_fmt)).otherwise(
-        f.unix_timestamp('Closed_Date', format=time_fmt) - f.unix_timestamp('Created_Date', format=time_fmt))
+        unix_timestamp('Closed_Date', format=time_fmt) - unix_timestamp('Created_Date', format=time_fmt))
     return df_311.withColumn("time_to_resolve", time_diff)
+
+
+def create_separate_day_month_year_col(df_311):
+    df_with_year = df_311.withColumn('Creation_Year', substring('Created_Date', 7, 4)).withColumn(
+        'Closing_Year', substring('Closed_Date', 7, 4))
+
+    time_fmt = "dd/MM/yyyy HH:mm:ss"
+    change_format_month = when(to_timestamp(df_311.Closed_Date, time_fmt).isNull(),
+                               substring('Closed_Date', 1, 2)).otherwise(substring('Closed_Date', 4, 2))
+    change_format_day = when(to_timestamp(df_311.Closed_Date, time_fmt).isNull(),
+                             substring('Closed_Date', 4, 2)).otherwise(substring('Closed_Date', 1, 2))
+
+    df_with_month = df_with_year.withColumn('Creation_Month', substring('Created_Date', 4, 2))
+    df_with_month = df_with_month.withColumn('Closing_Month', change_format_month)
+    df_with_month = df_with_month.withColumn("Creation_Month", when(col("Creation_Month") == '01', 'Jan')
+                                             .when(col("Creation_Month") == '02', 'Feb')
+                                             .when(col("Creation_Month") == '03', 'Mar')
+                                             .when(col("Creation_Month") == '04', 'Apr')
+                                             .when(col("Creation_Month") == '05', 'May')
+                                             .when(col("Creation_Month") == '06', 'Jun')
+                                             .when(col("Creation_Month") == '07', 'Jul')
+                                             .when(col("Creation_Month") == '08', 'Aug')
+                                             .when(col("Creation_Month") == '09', 'Sep')
+                                             .when(col("Creation_Month") == '10', 'Oct')
+                                             .when(col("Creation_Month") == '11', 'Nov')
+                                             .when(col("Creation_Month") == '12', 'Dec')
+                                             .otherwise('Unspecified'))
+
+    df_with_day = df_with_month.withColumn('Creation_Day', substring('Created_Date', 1, 2))
+    df_with_day = df_with_day.withColumn('Closing_Day', change_format_day)
+
+    df_with_time = df_with_day.withColumn('Creation_Time', substring('Created_Date', 12, 11)).withColumn(
+        'Closing_Time', substring('Closed_Date', 12, 11))
+
+    df_with_time = df_with_time.withColumn('Creation_Hour', when(substring('Creation_Time', 10, 2) == 'AM',
+                                                                 substring('Creation_Time', 1, 2)).otherwise(
+        substring('Creation_Time', 1, 2).cast(IntegerType()) + 12))
+
+    df_with_year_month_day = df_with_time.withColumn('Closing_Hour', when(substring('Closing_Time', 10, 2) == 'AM',
+                                                                          substring('Closing_Time', 1, 2)).otherwise(
+        substring('Closing_Time', 1, 2).cast(IntegerType()) + 12))
+
+    return df_with_year_month_day
