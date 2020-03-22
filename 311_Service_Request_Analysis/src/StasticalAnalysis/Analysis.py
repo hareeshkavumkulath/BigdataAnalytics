60a02181766c66cc1b21e7b2aa58afdbc31e5be7
Apoorv Semwal <apoorv.semwal20@gmail.com>
2020-03-22 01:58:37 -0400

    Added Utility Functions for basic Plotting and a Sample Hourly Analysis of Complaints

--- 311_Service_Request_Analysis/src/StasticalAnalysis/Analysis.py
+++ 311_Service_Request_Analysis/src/StasticalAnalysis/Analysis.py
@@ -1,6 +1,10 @@
 # Python source script for Analysis Part
+import Utilities as utilFor311
 
 
-def monthly_hourly_analysis(df_311):
+def monthly_hourly_analysis(df_with_year_month_day):
     # Insight 2 : Monthly and Hourly Analysis
-    return df_311
+    complaint_frequency_hour = df_with_year_month_day.groupby('Creation_Hour').count().orderBy(['Creation_Hour'],
+                                                                                               ascending=[0]).collect()
+    x, y = utilFor311.create_x_y_coordinates_for_group_by_results(complaint_frequency_hour, 'Creation_Hour')
+    utilFor311.plot_chart_x_y(x, y, "Frequency of complaints per hour", "Hour", "Frequency", range(0, 24, 1))
